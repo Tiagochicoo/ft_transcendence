4edd26404bedae960c1cf2daaad00092de7d1480
tpereira <tpereira@student.42lisboa.com>
2024-04-24 21:30:56 +0100

    creation of superuser at django startup

--- srcs/backend/entrypoint.sh
+++ srcs/backend/entrypoint.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 
-# Wait for the PostgreSQL server to be available
-echo "Waiting for postgres..."
+# Wait for the database to be ready
+echo "Waiting for database to start..."
 while ! nc -z db 5432; do
   sleep 0.1
 done
-echo "PostgreSQL started"
+echo "Database started"
 
 # Apply database migrations
 echo "Applying database migrations..."
 python manage.py migrate
 
-# Start your Django app
-exec "$@"
+# Create a superuser - this can be conditional or based on an environment variable
+if [ "$DJANGO_CREATE_SUPERUSER" = "True" ]; then
+    echo "Creating superuser..."
+    echo "from django.contrib.auth.models import User; User.objects.create_superuser('$POSTGRES_USER', '$POSTGRES_EMAIL', '$POSTGRES_PASSWORD')" | python manage.py shell
+fi
+
+# Start the Django application
+echo "Starting Django server..."
+python manage.py runserver 0.0.0.0:8000
