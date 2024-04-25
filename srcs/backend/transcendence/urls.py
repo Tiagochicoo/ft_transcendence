a40eb1e232085a71d27738b1100206f6ef28a876
tpereira <tpereira@student.42lisboa.com>
2024-04-25 19:43:59 +0100

    added user update and delete endpoints

--- srcs/backend/transcendence/urls.py
+++ srcs/backend/transcendence/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('create_user/', UserCreate.as_view(), name='create_user'),
 	path('users/', views.UserList.as_view(), name='users'),
 	path('users/<int:pk>/', views.UserDetail.as_view(), name='user_detail'),
+	path('users/<int:pk>/update/', views.UserUpdate.as_view(), name='user_update'),
+	path('users/<int:pk>/delete/', views.UserDelete.as_view(), name='user_delete'),
 ]
--- srcs/backend/transcendence/views.py
+++ srcs/backend/transcendence/views.py
@@ -26,4 +26,18 @@ class UserCreate(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class UserUpdate(APIView):
+    def put(self, request, pk, format=None):
+        user = User.objects.get(pk=pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserDelete(APIView):
+    def delete(self, request, pk, format=None):
+        user = User.objects.get(pk=pk)
+        if user.delete():
+            return Response("User deleted successfully from DB" ,status=status.HTTP_200_OK)
 
