0469a97fc85920815c4601c2e9979928516876e2
Edgar Marques <edgar.eu1@hotmail.com>
2024-04-25 22:07:04 +0100

    Create the Match model

--- srcs/backend/transcendence/models/__init__.py
+++ srcs/backend/transcendence/models/__init__.py
@@ -1,10 +1,12 @@
 from .friend_request import FriendRequest
+from .match import Match
 from .tournament_user import TournamentUser
 from .tournament import Tournament
 from .user import User
 
 __all__ = [
 	'FriendRequest',
+	'Match',
 	'TournamentUser',
 	'Tournament',
 	'User'
