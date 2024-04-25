90299f96415e4d3a752c2d17ebd5911d6a5d38ca
Edgar Marques <edgar.eu1@hotmail.com>
2024-04-25 21:18:19 +0100

    Create the FriendRequest model

--- srcs/backend/transcendence/models/__init__.py
+++ srcs/backend/transcendence/models/__init__.py
@@ -1,5 +1,7 @@
+from .friend_request import FriendRequest
 from .user import User
 
 __all__ = [
+	'FriendRequest',
 	'User'
 ]
---
+++ srcs/backend/transcendence/models/friend_request.py
@@ -0,0 +1,12 @@
+from django.db import models
+from .user import User
+
+class FriendRequest(models.Model):
+	user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_friend_requests')
+	user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_friend_requests')
+	was_accepted = models.BooleanField(default=False)
+	was_canceled = models.BooleanField(default=False)
+	was_refused = models.BooleanField(default=False)
+
+	def __str__(self):
+		return f"FriendRequest from {self.user1} to {self.user2}"
