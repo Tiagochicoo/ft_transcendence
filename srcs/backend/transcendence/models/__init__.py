67aaa78d5355d5eed9af8ed5e33559e48f2e0d4f
Edgar Marques <edgar.eu1@hotmail.com>
2024-04-25 21:58:42 +0100

    Create the TournamentUser model

--- srcs/backend/transcendence/models/__init__.py
+++ srcs/backend/transcendence/models/__init__.py
@@ -1,9 +1,11 @@
 from .friend_request import FriendRequest
+from .tournament_user import TournamentUser
 from .tournament import Tournament
 from .user import User
 
 __all__ = [
 	'FriendRequest',
+	'TournamentUser',
 	'Tournament',
 	'User'
 ]
