927bcfa135e64a94f2453810f5bec7fd3354f49c
ivo <ivo>
2024-04-30 18:15:56 +0100

    testcommit

--- srcs/backend/transcendence/urls.py
+++ srcs/backend/transcendence/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from django.urls import path
 from .views import UserCreate, UserList
-
+#
 urlpatterns = [
     path('create_user/', UserCreate.as_view(), name='create_user'),
 	path('users/', views.UserList.as_view(), name='users'),
