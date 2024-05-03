b7c39375e65b2bb4038262236789d56aee4780c7
Edgar Marques <edgar.eu1@hotmail.com>
2024-05-03 19:26:24 +0100

    Update the FriendsSection component data display

--- srcs/frontend/static/js/components/Sidebar/FriendsSection.js
+++ srcs/frontend/static/js/components/Sidebar/FriendsSection.js
@@ -44,9 +44,11 @@ export default class extends Abstract {
 	async getHtml() {
 		this.data = await Friends.getAll();
 
-		const accepted = this.data.filter(({ was_accepted }) => was_accepted)
+		const listAccepted = this.data.filter(({ was_accepted }) => was_accepted)
 			.map(({ id, user1, user2 }) => ({ id, user: (user1.id == 1) ? user2 : user1 }));
-		const invitations = this.data.filter(({ was_accepted, was_canceled, was_refused, user1 }) => !was_accepted && !was_canceled && !was_refused && (user1.id == 1))
+		const listReceived = this.data.filter(({ was_accepted, was_canceled, was_refused, user2 }) => !was_accepted && !was_canceled && !was_refused && (user2.id == 1))
+			.map(({ id, user1 }) => ({ id, user: user1 }));
+		 const listSent = this.data.filter(({ was_accepted, was_canceled, was_refused, user1 }) => !was_accepted && !was_canceled && !was_refused && (user1.id == 1))
 			.map(({ id, user2 }) => ({ id, user: user2 }));
 
 		return `
@@ -57,8 +59,8 @@ export default class extends Abstract {
 
 				<div id="friends-list" class="collapse">
 					<ul class="list-unstyled d-flex flex-column gap-2">
-						${accepted.map(({ id, user }) => `
-							<div class="sidebar-section-element d-flex justify-content-between gap-1 p-1 bg-light rounded">
+						${listAccepted.map(({ id, user }) => `
+							<div class="sidebar-section-element d-flex justify-content-between gap-1 p-1 bg-light rounded" data-friend-id="${id}">
 								<div class="d-flex align-items-center gap-1">
 									<img src="${user.avatar}" class="rounded-circle" />
 
@@ -79,17 +81,17 @@ export default class extends Abstract {
 
 				<div class="mt-2">
 					<button class="btn btn-toggle d-flex gap-2 align-items-center text-start text-white opacity-75 w-100 p-0 border-0 mb-2" data-bs-toggle="collapse" data-bs-target="#friends-received" aria-expanded="false">
-						Invitations
+						Invitations Received
 
 						<span class="badge text-bg-secondary">
-							${invitations.length}
+							${listReceived.length}
 						</span>
 					</button>
 
 					<div id="friends-received" class="collapse">
 						<ul class="list-unstyled d-flex flex-column gap-2">
-							${invitations.map(({ id, user }) => `
-								<div class="sidebar-section-element d-flex justify-content-between gap-1 p-1 bg-light rounded">
+							${listReceived.map(({ id, user }) => `
+								<div class="sidebar-section-element d-flex justify-content-between gap-1 p-1 bg-light rounded" data-friend-id="${id}">
 									<div class="d-flex align-items-center gap-1">
 										<img src="${user.avatar}" class="rounded-circle" />
 
@@ -105,6 +107,38 @@ export default class extends Abstract {
 
 										<button class="bg-transparent p-1 border-0" data-action="accept" data-id="${id}">
 											<i class="bi bi-check-circle-fill"></i>
+										</button>
+									</div>
+								</div>
+							`).join("")}
+						</ul>
+					</div>
+				</div>
+
+				<div class="mt-2">
+					<button class="btn btn-toggle d-flex gap-2 align-items-center text-start text-white opacity-75 w-100 p-0 border-0 mb-2" data-bs-toggle="collapse" data-bs-target="#friends-sent" aria-expanded="false">
+						Invitations Sent
+
+						<span class="badge text-bg-secondary">
+							${listSent.length}
+						</span>
+					</button>
+
+					<div id="friends-sent" class="collapse">
+						<ul class="list-unstyled d-flex flex-column gap-2">
+							${listSent.map(({ id, user }) => `
+								<div class="sidebar-section-element d-flex justify-content-between gap-1 p-1 bg-light rounded" data-friend-id="${id}">
+									<div class="d-flex align-items-center gap-1">
+										<img src="${user.avatar}" class="rounded-circle" />
+
+										<span class="lh-1">
+											${user.username}
+										<span>
+									</div>
+
+									<div class="d-flex align-items-center gap-1">
+										<button class="bg-transparent p-1 border-0" data-action="cancel" data-id="${id}">
+											<i class="bi bi-x-circle-fill"></i>
 										</button>
 									</div>
 								</div>
