9719a5a4030344d7a9ae86bc1706473acb335cd0
ivo <ivo>
2024-05-08 01:18:19 +0100

    chatroom_views

--- srcs/backend/transcendence/views/chatroom.py
+++ srcs/backend/transcendence/views/chatroom.py
@@ -3,41 +3,72 @@ from rest_framework.views import APIView
 from rest_framework import status
 from ..models import User as User
 from ..models import ChatRoom as ChatRoom
-#from ..serializers.serializers_chatroom import ChatRoomSerializer
-
+from ..serializers.serializers_chatroom import ChatRoomSerializer
 
 class ChatRoomCreate(APIView):
     def post(self, request, format=None):
-Post /chat_rooms
-
-class ChatRoomCancel(APIView):
-    def patch(self, request, format=None):
-Patch /chat_rooms/:chatRoomId/cancel
-
-class ChatRoomAccept(APIView):
-    def patch(self, request, format=None):
-Patch /chat_rooms/:chatRoomId/accept
-
-class ChatRoomRefuse(APIView):
-    def patch(self, request, format=None):
-Patch /chat_rooms/:chatRoomId/refuse
+        user1 = request.data.get('user1')
+        user2 = request.data.get('user2')
+         try:
+            chat_room = ChatRoom.objects.create(user1_id=user1, user2_id=user2)
+            serializer = ChatRoomSerializer(chat_room)
+            return JsonResponse({'success': True, 'data': serializer.data}, status=status.HTTP_200_OK)
+        except Exception as error:
+            return JsonResponse({'success': False}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class ChatRoomBlock(APIView):
-    def patch(self, request, format=None):
-Patch /chat_rooms/:chatRoomId/block
+    def patch(self, request, chatRoomId, format=None):
+        try:
+            chat_room = ChatRoom.objects.get(pk=chatRoomId)
+            chat_room.was_blocked = True
+            chat_room.save()
+            serializer = FriendRequestSerializer(chat_room)
+            return JsonResponse({'success': True, 'data': serializer.data}, status=status.HTTP_200_OK)
+        except Exception as error:
+            return JsonResponse({'success': False}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class ChatRoomUnblock(APIView):
-    def patch(self, request, format=None):
-Patch /chat_rooms/:chatRoomId/unblock
+    def patch(self, request, chatRoomId, format=None):
+        try:
+            chat_room = ChatRoom.objects.get(pk=chatRoomId)
+            chat_room.was_blocked = False
+            chat_room.save()
+            serializer = FriendRequestSerializer(chat_room)
+            return JsonResponse({'success': True, 'data': serializer.data}, status=status.HTTP_200_OK)
+        except Exception as error:
+            return JsonResponse({'success': False}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class ChatRoomCancel(APIView):
+    def patch(self, request, chatRoomId, format=None):
+
+class ChatRoomAccept(APIView):
+    def patch(self, request, chatRoomId, format=None):
+
+class ChatRoomRefuse(APIView):
+    def patch(self, request, chatRoomId, format=None):
+
+
+
 
 class ChatRoomMessages(APIView):
-    def patch(self, request, format=None):
-Patch /chat_rooms/:chatRoomId/messages
+    def get(self, request, chatRoomId, format=None):
+    def patch(self, request, chatRoomId, format=None):
 
 class ChatRoomDetails(APIView):
-    def get(self, request, format=None):
-Get /chat_rooms/:chatRoomId
+    def get(self, request, chatRoomId, format=None):
+        try:
+            chat_room = ChatRoom.objects.get(pk=chatRoomId)
+            serializer = FriendRequestSerializer(chat_room)
+            return JsonResponse({'success': True, 'data': serializer.data}, status=status.HTTP_200_OK)
+        except Exception as error:
+            return JsonResponse({'success': False}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class UserChatRoomDetails(APIView):
-    def get(self, request, format=None):
-Get /users/:userId/chat_rooms
+    def get(self, request, userId, format=None):
+        try:
+            user = User.objects.get(pk=userId)
+            chat_room = ChatRoom.objects.filter(user1=user) | ChatRoom.objects.filter(user2=user)
+            serializer = FriendRequestSerializer(chat_room, many=True)
+            return JsonResponse({'success': True, 'data': serializer.data}, status=status.HTTP_200_OK)
+        except Exception as error:
+            return JsonResponse({'success': False}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
