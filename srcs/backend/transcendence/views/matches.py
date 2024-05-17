7c83f50ada188574e87be0317b8a0e473aa4e5a5
Ivo patricio Freire marques <ifreire-@c1r20s14.42lisboa.com>
2024-05-17 13:56:39 +0100

    Match & tourn folders

---
+++ srcs/backend/transcendence/views/matches.py
@@ -0,0 +1,6 @@
+Post /matches
+PATCH /matches/:matchid/cancel    
+PATH /matches/:matchId/accept      
+PATCH /matches/:matchId/refuse      
+PATCH /matches/:matchId/finish     
+GET  /matches/:matchId
---
+++ srcs/backend/transcendence/views/tournaments.py
@@ -0,0 +1,5 @@
+Post /tournaments
+PATCH /tournaments/:tournamentId/start
+PATCH /tournaments/:tournamentId/finish
+GET /tournaments/:tournamentId
+GET /users/:userId/tournaments
