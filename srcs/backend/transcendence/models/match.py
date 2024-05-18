84c22e9370d8bdbce63c9d7ea4b0478863d2b11f
Anastacia <anaanestesia@gmail.com>
2024-05-18 09:48:52 +0100

    naive datetime warning solved

--- srcs/backend/transcendence/models/match.py
+++ srcs/backend/transcendence/models/match.py
@@ -1,11 +1,11 @@
 from django.db import models
 from .tournament import Tournament
 from .user import User
-from datetime import datetime
+from django.utils import timezone
 
 class Match(models.Model):
 	id = models.AutoField(primary_key=True)
-	created_on = models.DateTimeField(default=datetime.now)
+	created_on = models.DateTimeField(default=timezone.now)
 	user1 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='matches_as_user1')
 	user2 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='matches_as_user2')
 	tournament = models.ForeignKey(Tournament, on_delete=models.SET_NULL, null=True, related_name='matches')
