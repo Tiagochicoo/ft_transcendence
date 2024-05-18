9006c72a3f2271f1c4994048fc1ac3d441f9f5b1
Edgar Marques <edgar.eu1@hotmail.com>
2024-05-18 20:59:56 +0100

    Implement the 'Send Notification' functionality

--- srcs/frontend/static/js/services/index.js
+++ srcs/frontend/static/js/services/index.js
@@ -2,5 +2,6 @@ import generateSocket from "./generate-socket.js";
 import i18nextInit from "./i18next.js";
 import navigateTo from "./navigation.js";
 import renderPage from "./render.js";
+import sendNotification from "./notifications.js";
 
-export { generateSocket, i18nextInit, navigateTo, renderPage };
+export { generateSocket, i18nextInit, navigateTo, renderPage, sendNotification };
