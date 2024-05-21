e7d56ceb0ce738e4362c079140f5483340ff4379
tpereira <tpereira@student.42lisboa.com>
2024-05-21 20:42:07 +0100

    reset to migrations

--- srcs/backend/transcendence/models/user.py
+++ srcs/backend/transcendence/models/user.py
@@ -5,7 +5,7 @@ class User(models.Model):
 	email = models.EmailField(unique=True)
 	password = models.CharField(max_length=128)
 	username = models.CharField(max_length=50, unique=True)
-	avatar = models.ImageField(upload_to='avatars/')
+	avatar = models.ImageField(upload_to='avatars/', default='avatars/avatar.png')
 	num_games = models.IntegerField(default=0)
 	num_games_won = models.IntegerField(default=0)
 	num_tournaments = models.IntegerField(default=0)
