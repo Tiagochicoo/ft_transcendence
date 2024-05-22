707649002d3fee1b0d30753d29092d1d1ba0ca22
Anastacia <anaanestesia@gmail.com>
2024-05-22 11:15:27 +0100

    solving migrations errors

--- srcs/backend/transcendence/migrations/0012_alter_match_id.py
+++ srcs/backend/transcendence/migrations/0012_alter_match_id.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('transcendence', '0011_alter_user_email_alter_user_username'),
+        ('transcendence', '0003_alter_user_avatar'),
     ]
 
     operations = [
--- srcs/backend/transcendence/migrations/0015_merge_20240521_0904.py
+++ srcs/backend/transcendence/migrations/0015_merge_20240521_0904.py
@@ -6,7 +6,6 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('transcendence', '0013_message_created_at'),
         ('transcendence', '0014_alter_match_created_on'),
     ]
 
