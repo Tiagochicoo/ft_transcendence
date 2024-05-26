719559ccedd3a03ca958b71bfc04c9ab8dec5fa2
Edgar Marques <dev@weaccelerategrowth.com>
2024-05-26 22:28:13 +0100

    Add API routes for the Matches models

--- srcs/backend/transcendence/urls.py
+++ srcs/backend/transcendence/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views.chatroom import ChatRoomCreate, ChatRoomBlock, ChatRoomUnblock, ChatRoomMessages, ChatRoomDetails, UserChatRoomDetails
 from .views.friendrequest import FriendCreate, FriendCancel, FriendAccept, FriendRefuse, FriendDetails, UserFriendDetails
-from .views.match import MatchCreate, MatchDetail, MatchUpdate, MatchByTournament
+from .views.match import MatchCreate, MatchDetail, MatchUpdate, MatchCancel, MatchAccept, MatchRefuse, MatchByTournament, UserMatchDetails
 from .views.tournament import TournamentCreate, TournamentDetail, TournamentUpdate
 from .views.tournament_user import TournamentUserCreate, TournamentUserDetail
 from .views.user import UserList, UserDetails, UserLogin, UserUpdate, WhoAmI
@@ -33,10 +33,14 @@ urlpatterns = [
     path('chat_rooms/<int:chatRoomId>', ChatRoomDetails.as_view(), name='chat_room_details'),
     path('users/<int:userId>/chat_rooms', UserChatRoomDetails.as_view(), name='user_chat_room_details'),
 	
-    path('match/', MatchCreate.as_view(), name='create_match'),
-	path('match/<int:id>/', MatchDetail.as_view(), name='match_detail'),
-	path('match/update', MatchUpdate.as_view(), name='match_update'),
-	path('match/on-tournament/<int:tournament_id>/', MatchByTournament.as_view(), name='match_by_tournament'),
+    path('matches/', MatchCreate.as_view(), name='create_match'),
+    path('matches/<int:id>/', MatchDetail.as_view(), name='match_detail'),
+    path('matches/update', MatchUpdate.as_view(), name='match_update'),
+    path('matches/<int:MatchId>/cancel/', MatchCancel.as_view(), name='match_cancel'),
+    path('matches/<int:MatchId>/accept/', MatchAccept.as_view(), name='match_accept'),
+    path('matches/<int:MatchId>/refuse/', MatchRefuse.as_view(), name='match_refuse'),
+    path('matches/on-tournament/<int:tournament_id>/', MatchByTournament.as_view(), name='match_by_tournament'),
+    path('users/<int:userId>/matches/', UserMatchDetails.as_view(), name='user_match_details'),
 	
     path('tournament/', TournamentCreate.as_view(), name='create_tournament'),
 	path('tournament/update', TournamentUpdate.as_view(), name='tournament_update'),
