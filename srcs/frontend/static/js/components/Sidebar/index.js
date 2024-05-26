d878c22caf618a7eb65636e6d4c8858d0bc597aa
Edgar Marques <dev@weaccelerategrowth.com>
2024-05-26 04:13:56 +0100

    Update the Sidebar styling

--- srcs/frontend/static/js/components/Sidebar/index.js
+++ srcs/frontend/static/js/components/Sidebar/index.js
@@ -22,13 +22,15 @@ export default class extends Abstract {
 		}
 
 		return `
-			<div class="sidebar-wrapper">
-				<div class="sidebar-inner-wrapper position-absolute top-0 start-0 py-4 px-3 overflow-y-scroll rounded">
-					<h2 class="text-white lh-1 mb-4">
-						Menu
-					</h2>
+			<div class="sidebar-outter-wrapper">
+				<div class="sidebar-wrapper">
+					<div class="sidebar-inner-wrapper position-absolute top-0 start-0 h-100 w-100 py-4 px-3 overflow-y-scroll rounded">
+						<h2 class="text-white lh-1 mb-4">
+							Menu
+						</h2>
 
-					${await FriendsSection.getHtml()}
+						${await FriendsSection.getHtml()}
+					</div>
 				</div>
 			</div>
 		`;
