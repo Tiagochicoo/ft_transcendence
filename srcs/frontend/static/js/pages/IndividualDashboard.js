e70494a0d66ff09e5e11bac349856639a850069e
ivo <ivo>
2024-05-31 01:01:58 +0100

    update_individual_dashboard

--- srcs/frontend/static/js/pages/IndividualDashboard.js
+++ srcs/frontend/static/js/pages/IndividualDashboard.js
@@ -1,59 +1,90 @@
 import { Abstract } from "/static/js/components/index.js";
-import { PongData } from "/static/js/api/index.js";
+import { Users } from "/static/js/api/index.js";
+import { Matches } from "/static/js/api/index.js";
 
 export default class extends Abstract {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
+	}
 
-    this.params = props;
-    this.data;
-    this.user;
-    // we can use this to retreive data from database
-    this.userId = this.params.userId;
-  }
+	async addFunctionality() {}
 
-  generateTable() {
-    let table = `<table class="table table-hover text-center">
-						<thead class="table-secondary">
-							<tr>
-								<th scope="col">${i18next.t("individualDashboard.columns.id")}</th>
-								<th scope="col">${i18next.t("individualDashboard.columns.victory")}</th>
-								<th scope="col">${i18next.t("individualDashboard.columns.defeat")}</th>
-								<th scope="col">${i18next.t("individualDashboard.columns.opponent")}</th>
-								<th scope="col">${i18next.t("individualDashboard.columns.points")}</th>
-								<th scope="col">${i18next.t("individualDashboard.columns.total")}</th>
-							</tr>
-						</thead>
-						<tbody class="table-group-divider" style="border-top-color: #6c757d">`;
 
-    this.user.games.forEach((game) => {
-      table += `<tr>
-							<th scope="row">${game.gameId}</th>
-							<td>${game.victory ? "x" : " "}</td>
-							<td>${game.defeat ? "x" : " "}</td>
-							<td>${game.opponent}</td>
-							<td>${game.points}</td>
-							<td>${game.totalPoints}</td>
-						</tr>`;
-    });
-    table += "</tbody></table>";
-    return table;
-  }
-
-  async addFunctionality() {}
-
-  async getHtml() {
-    // fetching data mocked on db.json
-    this.data = await PongData.getMockedData();
-    // this.user = this.data.users.filter((user) => user.userId == this.userId)[0];
-	// mocked for now
-	this.user = this.data.users[0];
-
-    return `
-			<h1>
-				${i18next.t("individualDashboard.title")} - ${this.user.username}
-			</h1>
-			<div class="dashboard">${this.generateTable()}</div>
+	errorMessage() {
+		return `
+			<h1>Something went wrong</h1>
 		`;
-  }
+	}
+
+	async getHtml() {
+		// fetching data mocked on db.json
+		this.user = await Users.get(2);
+		if (!this.user.success)
+			return errorMessage;
+
+		var totalScore = 0;
+	
+		this.Matches = await Matches.getAll(2);
+		if (!this.Matches.success)
+			return errorMessage;
+		else
+		{
+			console.log(this.Matches);
+			console.log(this.user);
+			for (var i = 0; i < this.Matches.data.length; i++)
+			{
+				if (this.Matches.data[i].has_finished == true)
+				{
+					if (this.Matches.data[i].winner.username == this.user.data.username)
+					{
+						totalScore += 1;
+					}
+				}
+			}
+		}
+
+		return `
+			<h1>
+				${i18next.t("Personal Dashboard")} - ${this.user.data.username}
+			</h1>
+			
+			<div class="dashboard">
+				<table class="table table-hover text-center">
+					<thead class="table-secondary">
+						<tr>
+							<th scope="col">${i18next.t("Match_ID")}</th>
+							<th scope="col">${i18next.t("Opponent")}</th>
+							<th scope="col">${i18next.t("Winner")}</th>
+							<th scope="col">${i18next.t("Score")}</th>
+						</tr>
+					</thead>
+					<tbody class="table-group-divider" style="border-top-color: #6c757d">
+						<tr>
+							<td colspan="6" class="text-center">${i18next.t("Total Matches: ")}${this.Matches.data.length} with ${this.Matches.data.filter(game => game.has_finished !== true).length} Matches Canceled</td>
+						</tr>
+						${this.Matches.data
+							.filter(game => game.has_finished !== false)
+							.map(game => {
+								this.Matches.data.forEach(game => {
+								});
+								return `
+									<tr>
+										<th scope="row">${game.id}</th>
+										<td>${game.user2.username}</td>
+										<td>${game.winner.username}</td>
+										<td>${game.score}</td>
+									</tr>
+								`;
+							})
+						}
+					</tbody>
+					<tbody class="table-group-divider" style="border-top-color: #6c757d">
+						<tr>
+							<td colspan="6" class="text-center">${i18next.t("Total Wins: ")}${totalScore}</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		`;
+	}
 }
