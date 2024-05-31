cba8caabea1c0075ae59a7db5546c90c3ef385c2
Edgar Marques <edgar.eu1@hotmail.com>
2024-05-31 04:10:21 +0100

    Create the TournamentOpenMatch functionality for the frontend server side

--- srcs/frontend/server-services/socket-listeners/tournaments.js
+++ srcs/frontend/server-services/socket-listeners/tournaments.js
@@ -1,5 +1,9 @@
 const fetch = require("node-fetch");
 
+const matches = require("./matches.js");
+
+const { initGame } = matches;
+
 const getAllTournamentUsers = async (tournamentId) => {
   return fetch(`http://backend:8000/api/tournaments/${tournamentId}/tournament_users/`, {
     headers: {
@@ -11,18 +15,39 @@ const getAllTournamentUsers = async (tournamentId) => {
   });
 }
 
+const createTournamentMatches = async (tournamentId) => {
+  return fetch(`http://backend:8000/api/tournaments/${tournamentId}/matches/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-api-key': process.env.API_KEY,
+    },
+  }).then(response => {
+    return response.json();
+  });
+}
+
 // Initialize the tournament
-// First a countdown of 10s
+// First a countdown of 5s
 // Then initialize the matches
 const initTournament = async (io, tournamentId) => {
   await getAllTournamentUsers(tournamentId)
-    .then(resposeJson => {
-      if (!resposeJson.success) {
+    .then(responseJson => {
+      if (!responseJson.success) {
         throw new Error();
       }
-      resposeJson.data.forEach(tournamentUser => {
+      responseJson.data.forEach(tournamentUser => {
         io.emit(`tournament_start_${tournamentUser.user.id}`, tournamentUser);
       });
+      return createTournamentMatches(tournamentId);
+    }).then(matches => {
+      setTimeout(() => {
+        matches.data.forEach(match => {
+          io.emit(`tournament_open_match_${match.user1.id}`, match.id);
+          io.emit(`tournament_open_match_${match.user2.id}`, match.id);
+          initGame(io, match);
+        });
+      }, 5000);
     }).catch(error => {
       console.log(`Error sending the start notifications for the tournament ${tournamentId}:`, error);
     });
