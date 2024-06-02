2c1e02a052db5230d07e2a5cb7d78e73faf55710
Edgar Marques <edgar.eu1@hotmail.com>
2024-06-02 18:38:08 +0100

    Create the getProfile generator

--- srcs/frontend/static/js/generators/User.js
+++ srcs/frontend/static/js/generators/User.js
@@ -27,4 +27,38 @@ export default class extends Abstract {
 			</a>
 		`;
 	}
+
+	static getProfile(user) {
+		if (!user || !user.id) {
+			return '';
+		}
+
+		return `
+			<div class="user-profile d-flex align-items-center flex-wrap gap-2">
+				<img src="${MEDIA_URL}${user.avatar}" class="rounded-circle" alt="User avatar" />
+
+				<div class="user-profile d-flex flex-column">
+					<p class="font-weight-bold mb-0">
+						<strong>
+							${user.username}
+						</strong>
+					</p>
+
+					<p class="mb-0">
+						<strong>
+							Matches won:
+						</strong>
+						${user.num_games_won}/${user.num_games}
+					</p>
+
+					<p class="mb-0">
+						<strong>
+							Tournaments won:
+						</strong>
+						${user.num_tournaments_won}/${user.num_tournaments}
+					</p>
+				</div>
+			</div>
+		`;
+	}
 }
