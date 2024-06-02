a74d632b03864477e6ed2db9f5c9e1ba194f4508
Edgar Marques <edgar.eu1@hotmail.com>
2024-06-02 19:59:16 +0100

    Update the seed file

--- srcs/backend/transcendence/management/commands/seed.py
+++ srcs/backend/transcendence/management/commands/seed.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.hashers import make_password
 from django.core.management.base import BaseCommand
 from datetime import timedelta
+from django.db.models import Q
 from django.utils import timezone
 import random
 from ...models import ChatRoom as ChatRoom
@@ -25,7 +26,7 @@ class Command(BaseCommand):
             name = f'user{i}'
             User.objects.create(username=name, email=f'{name}@gmail.com', password=make_password(f'pass-{name}'))
 
-        all_users = User.objects.all()
+        all_users = User.objects.all().order_by('id')
 
         # Create Friend Requests for the First User
         for user in all_users[1:10]:
@@ -62,6 +63,18 @@ class Command(BaseCommand):
             Match.objects.create(created_on=date, tournament=tournament, user1=all_users[4], user2=all_users[6], was_accepted=True, has_finished=True, score=random.randint(1, 5), winner=all_users[4])
             Match.objects.create(created_on=date, tournament=tournament, user1=all_users[0], user2=all_users[4], was_accepted=True, has_finished=True, score=random.randint(1, 5), winner=all_users[0])
             date = today
+
+        # Update the Users metrics
+        for user in User.objects.all():
+            user_matches = Match.objects.filter(Q(user1=user) | Q(user2=user))
+            user.num_games = user_matches.count()
+            user.num_games_won = user_matches.filter(winner_id=user.id).count()
+            user.num_tournaments = user.tournament_users.count()
+            user.num_tournaments_won = 0
+            for tournament_user in user.tournament_users.all():
+                if (tournament_user.tournament.winner.id == user.id):
+                    user.num_tournaments_won += 1
+            user.save()
 
         # Log messages
         self.stdout.write(self.style.SUCCESS(f"Users created: {User.objects.count()}"))
