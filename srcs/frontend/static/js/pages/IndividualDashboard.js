79472b9a350a309d938c8047a88f2835a2d71579
Edgar Marques <edgar.eu1@hotmail.com>
2024-06-02 19:25:08 +0100

    Update the IndividualDashboard page #2

--- srcs/frontend/static/js/pages/IndividualDashboard.js
+++ srcs/frontend/static/js/pages/IndividualDashboard.js
@@ -19,8 +19,10 @@ export default class extends Abstract {
 			return invalidPage();
 		}
 		this.user = response.data.user;
-		this.matches = response.data.matches.filter(({ has_finished }) => has_finished);
-		this.tournaments = response.data.tournaments;
+		this.matches = response.data.matches
+			.filter(({ has_finished }) => has_finished)
+			.sort(({ id }) => id);
+		this.tournament_users = response.data.tournament_users.sort(({ id }) => id);;
 
 		let numWins = 0;
 		for (const match of this.matches) {
@@ -31,47 +33,63 @@ export default class extends Abstract {
 			}
 		}
 
+		const parseDate = (rawDate) => {
+			const date = new Date(rawDate);
+			return date.toLocaleDateString();
+		}
+
 		return `
 			<h1>
-				${i18next.t("Personal Dashboard")}
+				${i18next.t("dashboard.individual.title")}
 			</h1>
 
 			${User.getProfile(this.user)}
 
-			<div class="dashboard">
+			<div class="matches-table mt-4">
 				<table class="table table-hover text-center">
 					<thead class="table-secondary">
 						<tr>
-							<th scope="col">${i18next.t("Match_ID")}</th>
-							<th scope="col">${i18next.t("Opponent")}</th>
-							<th scope="col">${i18next.t("Winner")}</th>
-							<th scope="col">${i18next.t("Score")}</th>
+							<th colspan="5"">${i18next.t("dashboard.matches")}</th>
+						</tr>
+
+						<tr>
+							<th scope="col">ID</th>
+							<th scope="col">${i18next.t("dashboard.date")}</th>
+							<th scope="col">${i18next.t("dashboard.opponent")}</th>
+							<th scope="col">${i18next.t("dashboard.winner")}</th>
+							<th scope="col">${i18next.t("dashboard.score")}</th>
 						</tr>
 					</thead>
-					<tbody class="table-group-divider" style="border-top-color: #6c757d">
-						<tr>
-							<td colspan="6" class="text-center">
-								${i18next.t("Total Matches: ")}${this.matches.length}
-							</td>
-						</tr>
 
-						${this.matches.map(match => `
-							<tr>
-								<th scope="row">${match.id}</th>
-								<td>${match.user1.id == this.user.id ? match.user2.username : match.user1.username}</td>
-								<td>${match.winner.username}</td>
-								<td>${match.score}</td>
-							</tr>
-						`).join("")}
+					<tbody class="table-group-divider">
+						${this.matches.map(match => {
+							const isWinner = (match.winner.id == this.user.id);
+							return `
+								<tr class="${isWinner ? 'won' : 'lost'}">
+									<th scope="row">${match.id}</th>
+									<td>${parseDate(match.created_on)}</td>
+									<td>${match.user1.id == this.user.id ? match.user2.username : match.user1.username}</td>
+									<td>${match.winner.username}</td>
+									<td>${isWinner ? match.score : -match.score}</td>
+								</tr>
+							`
+						}).join("")}
 					</tbody>
 
-					<tbody class="table-group-divider" style="border-top-color: #6c757d">
+					<tfoot class="table-group-divider">
 						<tr>
-							<td colspan="6" class="text-center">
-								${i18next.t("Total Wins: ")}${numWins}
+							<td colspan="2" class="text-center">
+								${i18next.t("dashboard.totalMatches")}${this.matches.length}
+							</td>
+
+							<td colspan="1">
+							</td>
+
+							<td colspan="2" class="text-center">
+								${i18next.t("dashboard.totalWins")}${numWins}
 							</td>
 						</tr>
-					</tbody>
+					</tfoot>
 				</table>
 			</div>
 		`;
